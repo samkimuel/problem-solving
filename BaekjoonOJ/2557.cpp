191c24da02b60825e423d2f32f6c75f035d0604c
Sam Myunghwan Kim <samkim2626@gmail.com>
2020-04-17 15:44:54 +0900

    2557

---
+++ BaekjoonOJ/2557.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file 2557.cpp
+ * @brief Hello World! 출력
+ * @author Sam Kim (samkim2626@gmail.com)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << '\n';
+
+    return 0;
+}
