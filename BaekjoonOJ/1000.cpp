8fd11f9d1c37e456796b18dd1042bfcbf1c58953
Sam Myunghwan Kim <samkim2626@gmail.com>
2020-04-17 22:22:11 +0900

    1000

---
+++ BaekjoonOJ/1000.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file 1000.cpp
+ * @brief A+B
+ * @author Sam Kim (samkim2626@gmail.com)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+
+    cin >> a >> b;
+    cout << a + b << '\n';
+
+    return 0;
+}
