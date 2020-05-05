38d715e91a4e82987cd4abe158d2020b6ef30761
Sam Myunghwan Kim <samkim2626@gmail.com>
2020-05-05 22:19:45 +0900

    1929

---
+++ BaekjoonOJ/1929.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file 1929.cpp
+ * @brief 소수 구하기
+ * @author Sam Kim (samkim2626@gmail.com)
+ * 
+ * 에라토스테네스의 체(Sieve of Eratosthenes) - O(NloglogN)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1000000;
+bool check[MAX + 1];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    check[0] = check[1] = true;
+    for (int i = 2; i * i <= MAX; i++)
+    {
+        if (check[i] == false)
+        {
+            for (int j = i * 2; j <= MAX; j += i)
+            {
+                check[j] = true;
+            }
+        }
+    }
+
+    int m, n;
+    cin >> m >> n;
+    for (int i = m; i <= n; i++)
+    {
+        if (check[i] == false)
+        {
+            cout << i << '\n';
+        }
+    }
+
+    return 0;
+}
