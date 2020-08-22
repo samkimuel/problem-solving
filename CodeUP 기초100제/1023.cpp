2c52eaa1ecdc45b92729af12880a6d8f1c843e7c
Myunghwan Kim <samkim2626@gmail.com>
2020-08-22 16:14:43 +0900

    코드업 기초100제

---
+++ CodeUP 기초100제/1023.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file 1023.cpp
+ * @brief 실수 1개 입력받아 부분별로 출력하기
+ * @author Sam Kim (samkim2626@gmail.com)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    scanf("%d.%d", &a, &b);
+    printf("%d\n%d\n", a, b);
+
+    return 0;
+}
