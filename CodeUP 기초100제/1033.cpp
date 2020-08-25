461b07fba40e3b72228e3c0c0fd0ccaa747b5041
Myunghwan Kim <samkim2626@gmail.com>
2020-08-25 23:35:57 +0900

    코드업 기초100제

---
+++ CodeUP 기초100제/1033.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file 1033.cpp
+ * @brief 10진 정수 입력받아 16진수로 출력하기2
+ * @author Sam Kim (samkim2626@gmail.com)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%X\n", n);
+
+    return 0;
+}
