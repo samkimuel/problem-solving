f7b930801f800efc7d2fe4c65da7329aed1fa284
Myunghwan Kim <samkim2626@gmail.com>
2020-08-26 09:13:56 +0900

    코드업 기초100제

---
+++ CodeUP 기초100제/1034.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file 1034.cpp
+ * @brief 8진 정수 1개 입력받아 10진수로 출력하기
+ * @author Sam Kim (samkim2626@gmail.com)
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%o", &n);
+    printf("%d\n", n);
+
+    return 0;
+}
