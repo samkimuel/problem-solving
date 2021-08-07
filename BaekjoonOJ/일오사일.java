52160774817fdda14009ac375191a861ca565a74
samkimuel <samkim2626@gmail.com>
2021-08-08 02:54:26 +0900

    1541: 문자열

---
+++ BaekjoonOJ/일오사일.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 일오사일 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split("-");
+
+        int answer = 0;
+
+        // 첫 번째 - 보기전까지 다 더하기
+        String[] sFirst = input[0].split("\\+");
+        for (int i = 0; i < sFirst.length; i++) {
+            answer += Integer.parseInt(sFirst[i]);
+        }
+
+        for (int i = 1; i < input.length; i++) {
+            String[] strings = input[i].split("\\+");
+            for (int j = 0; j < strings.length; j++) {
+                answer -= Integer.parseInt(strings[j]);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
